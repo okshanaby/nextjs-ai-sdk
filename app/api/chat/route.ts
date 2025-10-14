5bc708275fe1ea49078ba77ac879e2314aaa1643
OkkashaAlly <steamnature006@gmail.com>
2025-10-14 22:42:08 +0300

    8 - Prompt Engineering

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -11,13 +11,30 @@ export async function POST(request: Request) {
 
     const result = streamText({
       model: google("gemini-2.5-flash"),
-      messages: convertToModelMessages(messages), // use user input
+      messages: [
+        // {
+        //   role: "system",
+        //   content:
+        //     "You are a coding assistant. Keep answers not more thatn 3 sentences.",
+        // },
+        {
+          role: "system",
+          content: "Convert user questions about react into code samples",
+        },
+        { role: "user", content: "How to toggle a boolean" },
+        {
+          role: "assistant",
+          content:
+            "const [isOpen, setIsOpen] = useState(false);\n\nconst toggle = () => setIsOpen(!isOpen);",
+        },
+        ...convertToModelMessages(messages),
+      ],
     });
 
     return result.toUIMessageStreamResponse();
   } catch (error) {
     console.log("🚀 ~ POST ~ error:", error);
-    
+
     return Response.json(
       { success: false, error: "Failed to stream messages" },
       { status: 500 }
