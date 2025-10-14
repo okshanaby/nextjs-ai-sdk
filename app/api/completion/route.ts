6fba05e506536463329a3d9b2fbfc5400ef03df1
OkkashaAlly <steamnature006@gmail.com>
2025-10-14 20:34:58 +0300

    3 - Generate Text

---
+++ app/api/completion/route.ts
@@ -0,0 +1,31 @@
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { generateText } from "ai";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const google = createGoogleGenerativeAI({
+    apiKey: process.env.GOOGLE_API_KEY || "",
+  });
+
+  try {
+    const { text } = await generateText({
+      // model: openai("gpt-4.1-nano"),
+      // prompt: "Explain what is LLM in simple terms",
+      // providerOptions: {
+      //   openai: { apiKey: process.env.OPENAI_API_KEY || "" },
+      // },
+
+      model: google("gemini-2.5-flash"),
+      prompt: "Write a vegetarian lasagna recipe for 4 people.",
+    });
+
+    return NextResponse.json({ success: true, text });
+  } catch (error) {
+    console.log("🚀 ~ POST ~ error:", error);
+
+    return NextResponse.json(
+      { success: false, error: "Failed to generate text" },
+      { status: 500 }
+    );
+  }
+}
