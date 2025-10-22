789e353a07bfcd2bf7b3284a24923c1c5c107eaf
okshanaby <steamnature006@gmail.com>
2025-10-22 13:01:58 +0300

    25 - Reasoning

---
+++ app/api/reasoning/route.ts
@@ -0,0 +1,34 @@
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { convertToModelMessages, streamText, UIMessage } from "ai";
+
+export async function POST(request: Request) {
+  try {
+    const { messages }: { messages: UIMessage[] } = await request.json();
+
+    const google = createGoogleGenerativeAI({
+      apiKey: process.env.GOOGLE_API_KEY || "",
+    });
+
+    const result = streamText({
+      model: google("gemini-2.5-flash"),
+      messages: convertToModelMessages(messages),
+      providerOptions: {
+        google: {
+          reasoningSummary: "auto",
+          reasoningEffort: "low",
+        },
+      },
+    });
+
+    return result.toUIMessageStreamResponse({
+      sendReasoning: true,
+    });
+  } catch (error) {
+    console.log("🚀 ~ POST ~ error:", error);
+
+    return Response.json(
+      { success: false, error: "Failed to stream messages" },
+      { status: 500 }
+    );
+  }
+}
